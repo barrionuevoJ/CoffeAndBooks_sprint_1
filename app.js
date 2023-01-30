63bb68a83a8e0d32b53fd16a17838315e754e0f8
TomiBustamante <92278525+Tomi1382@users.noreply.github.com>
2023-01-30 10:26:51 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const app = express();
+
+const path = require('path');
+
+const port = 3030
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.sendFile(path.resolve('./views/home.html')))
+ 
+app.listen(port, () => console.log(`El servidor funcionando en el puerto ${port}`))
+
+
+
+
+
+
